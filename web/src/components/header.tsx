71db1b15d432cbe9ad96b89e10f21089052b0279
haydenfish <hayden.fish@icloud.com>
2024-05-11 21:34:34 -0700

    Added next/link component

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
 export default function Header() {
   return (
     <>
       <header className="py-6 px-8 bg-green-700">
-        <a href="/">Home</a>
+        <Button variant="ghost">
+          <Link href="/">Home</Link>
+        </Button>
       </header>
       <hr></hr>
     </>

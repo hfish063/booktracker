ecbc97e04eac5250dd58de858c796c03e1741bd0
haydenfish <hayden.fish@icloud.com>
2024-05-12 04:24:51 -0700

    Styling changes

--- web/src/components/app/header.tsx
+++ web/src/components/app/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <>
-      <header className="py-6 px-8">
+      <header className="py-6 px-8 shadow-md">
         <h1 className="text-5xl font-extrabold dark:text-black">
           Book<span className="text-green-600">tracker</span>
         </h1>
--- web/src/components/app/navbar.tsx
+++ web/src/components/app/navbar.tsx
@@ -4,16 +4,24 @@ import { Button } from "../ui/button";
 export default function NavBar() {
   return (
     <>
-      <header className="flex flex-col border-r-2">
+      <header className="flex flex-col bg-green-500">
         <Link href="/" className="hover:bg-sky-100">
-          <Button className="py-6 px-8 w-full" variant="link">
+          <Button
+            className="hover:text-sky-800 text-white py-6 px-8 w-full"
+            variant="link"
+          >
             Home
           </Button>
+          <hr />
         </Link>
         <Link href="/list" className="hover:bg-sky-100">
-          <Button className="py-6 px-8 w-full" variant="link">
+          <Button
+            className="hover:text-sky-800 text-white py-6 px-8 w-full"
+            variant="link"
+          >
             Reading Lists
           </Button>
+          <hr />
         </Link>
       </header>
       <hr></hr>

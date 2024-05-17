0b08e3c38ab4d4a95f1aad61175adbeb930abb3c
haydenfish <hayden.fish@icloud.com>
2024-05-17 14:12:16 -0700

    Removed unused class attributes

--- web/src/app/list/page.tsx
+++ web/src/app/list/page.tsx
@@ -4,7 +4,7 @@ import ReadingListWrapper from "@/components/reading-list/wrapper";
 export default async function List() {
   const readingLists = await getAllReadingLists();
   return (
-    <div className="flex flex-col gap 4">
+    <div className="flex flex-col">
       <h1 className="text-3xl font-semibold">Reading List Collection</h1>
       <ReadingListWrapper data={readingLists} />
     </div>
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap 4">
+    <div className="flex flex-col">
       <h1 className="text-3xl font-semibold">Welcome</h1>
       <Link href="/list">
         <Button>Get Started</Button>

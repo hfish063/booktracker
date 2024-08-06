5d4ae25a3d7013e6744573cae80ed9fcb64fd30c
haydenfish <hayden.fish@icloud.com>
2024-08-06 10:36:02 -0700

    Added navbar item for CompleteList page

--- web/src/components/misc/navbar.tsx
+++ web/src/components/misc/navbar.tsx
@@ -23,6 +23,15 @@ export default function NavBar() {
           </Button>
           <hr />
         </Link>
+        <Link href="/list" className="hover:bg-sky-100">
+          <Button
+            className="hover:text-sky-800 text-white py-6 px-8 w-full"
+            variant="link"
+          >
+            Completed Books
+          </Button>
+          <hr />
+        </Link>
       </header>
       <hr></hr>
     </>
